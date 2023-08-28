b29d2885aa450f390029984e1e3aea11710191a7
Garima Pundir <99747567+garimapundir29@users.noreply.github.com>
2023-08-28 17:26:23 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,7 @@
+ const typed = new Typed('.multiple-text',{
+    strings: ["", 'Software Developer', 'Full Stack Developer', 'C++ Programmer', 'Tech Enthusiastic', 'Problem Solver'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true
+ });
